351561d6e84fb7dc205aaa3e1035deb409a8c965
Varun Kushwah <varunkushwah500notes@gmail.com>
2026-02-25 17:17:22 +0530

    created draw screen

--- lib/Features/draw/presentation/drawscreen.dart
+++ lib/Features/draw/presentation/drawscreen.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'models/stroke.dart';
+
 class Drawscreen extends StatefulWidget {
-  const Drawscreen ({super.key});
+  const Drawscreen({super.key});
 
   @override
   State<Drawscreen> createState() => _DrawscreenState();
@@ -12,9 +13,71 @@ class _DrawscreenState extends State<Drawscreen> {
   List<Stroke> _strokes = [];
   List<Stroke> _redoStockes = [];
   List<Offset> _currentPoints = [];
-  Color _selectedColor = Colors.black
+  Color _selectedColor = Colors.black;
+  double _brushSize = 4.0;
   @override
-  Widget build(BuildContext context){
-    return const Placeholder();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Draw Your Dream"),
+      ),
+      body: Column(
+        children: [
+          GestureDetector(
+            onPanStart: (details) {
+              setState(() {
+                _currentPoints.add(details.localPosition);
+              });
+            },
+            onPanUpdate: (details) {
+              setState(() {
+                _currentPoints.add(details.localPosition);
+              });
+            },
+            onPanEnd: (details) {
+              setState(() {
+                _strokes.add(Stroke(
+                    points: List.from(_currentPoints),
+                    color: _selectedColor,
+                    brushSize: _brushSize));
+              });
+              _currentPoints = [];
+              _redoStockes = [];
+            },
+            child: CustomPaint(
+              painter: DrawPainter(
+                  strokes: _strokes,
+                  currentPoints: _currentPoints,
+                  currentColor: _selectedColor,
+                  currentBrushSize: _brushSize),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class DrawPainter extends CustomPainter {
+  final List<Stroke> strokes;
+  final List<Offset> currentPoints;
+  final Color currentColor;
+  final double currentBrushSize;
+
+  DrawPainter(
+      {super.repaint,
+        required this.strokes,
+        required this.currentPoints,
+        required this.currentColor,
+        required this.currentBrushSize});
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    throw UnimplementedError();
   }
 }
--- lib/Features/draw/presentation/models/stroke.dart
+++ lib/Features/draw/presentation/models/stroke.dart
@@ -3,6 +3,6 @@ class Stroke{
   final List<Offset> points;
   final Color color;
   final double brushSize;
-  Stroke({required this.points,required this.color, required this.brushSize})
+  Stroke({required this.points,required this.color, required this.brushSize});
 
 }
